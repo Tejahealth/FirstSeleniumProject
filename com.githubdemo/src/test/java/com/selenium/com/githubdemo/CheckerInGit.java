921180d93fdbe1925357c33a629e44cf561e7049
admin <gsantosh@icloud.com>
2015-07-25 21:20:40 +0100

    plz chek in online

---
+++ com.githubdemo/src/test/java/com/selenium/com/githubdemo/CheckerInGit.java
@@ -0,0 +1,11 @@
+package com.selenium.com.githubdemo;
+
+import org.testng.annotations.Test;
+
+public class CheckerInGit {
+
+	@Test
+	public void check(){
+		System.out.println("got update");
+}
+}
